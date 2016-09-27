238089e214b4d5b56b3b770d337f39eee8004051
zhongguo <zhongguo@creditease.cn>
2016-09-27 13:26:15 +0800

    fun Captcha to Query

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package dama2
 import (
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -24,12 +26,17 @@ func (p *Result) String() string {
 	return string(b)
 }
 
+var Instance *Dama2Client
+
 type Dama2Client struct {
 	client         *http.Client
 	key            string
 	auth           string
 	lastUpdateAuth time.Time
 	lock           *sync.Mutex
+	uname          string
+	pwd            string
+	appid          string
 }
 
 func (p *Dama2Client) updateAuth(a string) {
@@ -39,13 +46,16 @@ func (p *Dama2Client) updateAuth(a string) {
 	}
 }
 
-func NewDama2Client(k string) *Dama2Client {
+func NewDama2Client(k, uname, pwd, appid string) *Dama2Client {
 	return &Dama2Client{
 		client:         &http.Client{},
 		key:            k,
 		auth:           "",
 		lastUpdateAuth: time.Now(),
 		lock:           &sync.Mutex{},
+		uname:          uname,
+		pwd:            pwd,
+		appid:          appid,
 	}
 }
 
@@ -58,26 +68,30 @@ func (p *Dama2Client) getKey8() []byte {
 	return key8
 }
 
-func (p *Dama2Client) Init(appid, uname, pwd string) error {
+func (p *Dama2Client) Init() error {
 	p.lock.Lock()
 	defer p.lock.Unlock()
-	var err error
-	_, err = p.PreAuth()
-	if err != nil {
-		return err
-	}
-	_, err = p.Login(appid, uname, pwd, p.auth)
-	if err != nil {
-		return err
+	if len(p.auth) == 0 || time.Now().Sub(p.lastUpdateAuth).Minutes() > 5 {
+		var err error
+		_, err = p.PreAuth()
+		if err != nil {
+			return err
+		}
+		_, err = p.Login(p.appid, p.uname, p.pwd, p.auth)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
-func (p *Dama2Client) Captcha(img []byte, format string, codeType int, appid, uname, pwd string) (string, error) {
+func (p *Dama2Client) Query(img []byte, codeType, format string) (string, error) {
 	if len(p.auth) == 0 || (time.Now().Sub(p.lastUpdateAuth).Minutes() > 5) {
-		p.Init(appid, uname, pwd)
+		p.Init()
 	}
-	decode, err := p.Decode(img, format, p.auth, codeType, 0)
+	codeTypeInt, _ := strconv.Atoi(codeType)
+	fmt.Println(p.auth)
+	decode, err := p.Decode(img, format, p.auth, codeTypeInt, 0)
 	if err != nil {
 		return "", err
 	}
@@ -87,6 +101,7 @@ func (p *Dama2Client) Captcha(img []byte, format string, codeType int, appid, un
 		n += 1
 		if n > 5 {
 			tc.Stop()
+			break
 		}
 		ret, err := p.GetResult(p.auth, decode.Id)
 		if err == nil && ret.Ret == "0" {
--- dama2_test.go
+++ dama2_test.go
@@ -3,18 +3,31 @@ package dama2
 import (
 	"crypto/md5"
 	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
 func TestEncode(t *testing.T) {
 	uname := "user"
-	pwd := "test"
+	pwd := "password"
+	appid := "123"
 	preauth := "1234567890abcdef1234567890abcdef"
-	p := NewDama2Client("9503ce045ad14d83ea876ab578bd3184")
+	p := NewDama2Client("2ed9f7b2466caf9ff3f8d3109ca16530", uname, pwd, appid)
 	src := fmt.Sprintf("%s\n%s\n%s", preauth, uname, fmt.Sprintf("%x", md5.Sum([]byte(pwd))))
 	enc, _ := DesEncrypt([]byte(src), p.getKey8())
-	if fmt.Sprintf("%x", enc) != "a733506fda6e182300d34a1bcca568d8a733506fda6e182300d34a1bcca568d8f6368183d9f688b7a43c783d0184850751f1d013925667735ddd457beaa8da8010b74e6f64b15c4d" {
-		t.Error()
+	if fmt.Sprintf("%x", enc) != "55cdb9bd553232859bb7b0077dc51e3255cdb9bd553232859bb7b0077dc51e3221d4157f116dff5d30d57ca592186a776474a14edaf4250db7b2a6696eba226980ac6fa145ea789b9bebbda4cdc16b8f" {
+		t.Error(fmt.Sprintf("%x", enc))
 		return
 	}
+	img, err := ioutil.ReadFile("createVerifyCode.jpeg")
+	if err != nil {
+		t.Error(err)
+	}
+	format := "jpeg"
+	codeType := "90"
+	ret, err := p.Query(img, codeType, format)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(ret)
 }
